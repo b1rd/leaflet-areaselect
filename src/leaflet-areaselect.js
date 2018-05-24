09b8c112fb033aa36b45084464249d1494e87dd8
Alyona <notverysmartbird@gmail.com>
2018-05-25 01:41:01 +0300

    set up frame params

--- src/leaflet-areaselect.js
+++ src/leaflet-areaselect.js
@@ -1,9 +1,14 @@
+// import L from 'leaflet'
+import './leaflet-areaselect.scss'
 L.AreaSelect = L.Class.extend({
-    includes: L.Mixin.Events,
+    includes: L.Evented.prototype,
     
     options: {
         width: 200,
         height: 300,
+        mmWidth: 297,
+        mmHeight: 210,
+        measureUnits: 'мм',
         keepAspectRatio: false,
     },
 
@@ -12,6 +17,9 @@ L.AreaSelect = L.Class.extend({
         
         this._width = this.options.width;
         this._height = this.options.height;
+        this._measureUnits = this.options.measureUnits
+        this._mmWidth = this.options.mmWidth
+        this._mmHeight = this.options.mmHeight
     },
     
     addTo: function(map) {
@@ -61,76 +69,74 @@ L.AreaSelect = L.Class.extend({
         if (!!this._container)
             return;
         
-        this._container = L.DomUtil.create("div", "leaflet-areaselect-container", this.map._controlContainer)
-        this._topShade = L.DomUtil.create("div", "leaflet-areaselect-shade leaflet-control", this._container);
-        this._bottomShade = L.DomUtil.create("div", "leaflet-areaselect-shade leaflet-control", this._container);
-        this._leftShade = L.DomUtil.create("div", "leaflet-areaselect-shade leaflet-control", this._container);
-        this._rightShade = L.DomUtil.create("div", "leaflet-areaselect-shade leaflet-control", this._container);
-        
-        this._nwHandle = L.DomUtil.create("div", "leaflet-areaselect-handle leaflet-control", this._container);
-        this._swHandle = L.DomUtil.create("div", "leaflet-areaselect-handle leaflet-control", this._container);
-        this._neHandle = L.DomUtil.create("div", "leaflet-areaselect-handle leaflet-control", this._container);
-        this._seHandle = L.DomUtil.create("div", "leaflet-areaselect-handle leaflet-control", this._container);
-        
-        this._setUpHandlerEvents(this._nwHandle);
-        this._setUpHandlerEvents(this._neHandle, -1, 1);
-        this._setUpHandlerEvents(this._swHandle, 1, -1);
-        this._setUpHandlerEvents(this._seHandle, -1, -1);
+        this._container = L.DomUtil.create('div', 'leaflet-areaselect-container', this.map._controlContainer)
+        this._topShade = L.DomUtil.create('div', 'areaselect__shade leaflet-control', this._container)
+        this._bottomShade = L.DomUtil.create('div', 'areaselect__shade leaflet-control', this._container)
+        this._leftShade = L.DomUtil.create(
+            'div',
+            'areaselect__shade areaselect__shade_left leaflet-control',
+            this._container
+        )
+        this._rightShade = L.DomUtil.create(
+            'div',
+            'areaselect__shade areaselect__shade_right leaflet-control',
+            this._container
+        )
+    
+        this._topBorder = L.DomUtil.create(
+            'div',
+            'areaselect__border areaselect__border_top leaflet-control',
+            this._container
+        )
+        this._bottomBorder = L.DomUtil.create(
+            'div',
+            'areaselect__border areaselect__border_bottom leaflet-control',
+            this._container
+        )
+        this._leftBorder = L.DomUtil.create(
+            'div',
+            'areaselect__border areaselect__border_left leaflet-control',
+            this._container
+        )
+        this._rightBorder = L.DomUtil.create(
+            'div',
+            'areaselect__border areaselect__border_right leaflet-control',
+            this._container
+        )
+    
+        this._mmText = L.DomUtil.create('span', 'areaselect__text leaflet-control', this._container)
+        this._mmWidthText = L.DomUtil.create('span', 'areaselect__text leaflet-control', this._container)
+        this._mmHeightText = L.DomUtil.create('span', 'areaselect__text leaflet-control', this._container)
+        this._mmText.textContent = this._measureUnits
+        this._mmWidthText.textContent = this._mmWidth
+        this._mmHeightText.textContent = this._mmHeight
+    
+        this._nwHandle = L.DomUtil.create(
+            'div',
+            'areaselect__handle areaselect__handle_nw leaflet-control',
+            this._container
+        )
+        this._swHandle = L.DomUtil.create(
+            'div',
+            'areaselect__handle areaselect__handle_sw leaflet-control',
+            this._container
+        )
+        this._neHandle = L.DomUtil.create(
+            'div',
+            'areaselect__handle areaselect__handle_ne leaflet-control',
+            this._container
+        )
+        this._seHandle = L.DomUtil.create(
+            'div',
+            'areaselect__handle areaselect__handle_se leaflet-control',
+            this._container
+        )
         
         this.map.on("moveend", this._onMapChange, this);
         this.map.on("zoomend", this._onMapChange, this);
         this.map.on("resize", this._onMapResize, this);
         
         this.fire("change");
-    },
-    
-    _setUpHandlerEvents: function(handle, xMod, yMod) {
-        xMod = xMod || 1;
-        yMod = yMod || 1;
-        
-        var self = this;
-        function onMouseDown(event) {
-            event.stopPropagation();
-            self.map.dragging.disable();
-            L.DomEvent.removeListener(this, "mousedown", onMouseDown);
-            var curX = event.pageX;
-            var curY = event.pageY;
-            var ratio = self._width / self._height;
-            var size = self.map.getSize();
-            
-            function onMouseMove(event) {
-                if (self.options.keepAspectRatio) {
-                    var maxHeight = (self._height >= self._width ? size.y : size.y * (1/ratio) ) - 30;
-                    self._height += (curY - event.originalEvent.pageY) * 2 * yMod;
-                    self._height = Math.max(30, self._height);
-                    self._height = Math.min(maxHeight, self._height);
-                    self._width = self._height * ratio;
-                } else {
-                    self._width += (curX - event.originalEvent.pageX) * 2 * xMod;
-                    self._height += (curY - event.originalEvent.pageY) * 2 * yMod;
-                    self._width = Math.max(30, self._width);
-                    self._height = Math.max(30, self._height);
-                    self._width = Math.min(size.x-30, self._width);
-                    self._height = Math.min(size.y-30, self._height);
-                    
-                }
-                
-                curX = event.originalEvent.pageX;
-                curY = event.originalEvent.pageY;
-                self._render();
-            }
-            function onMouseUp(event) {
-                self.map.dragging.enable();
-                L.DomEvent.removeListener(self.map, "mouseup", onMouseUp);
-                L.DomEvent.removeListener(self.map, "mousemove", onMouseMove);
-                L.DomEvent.addListener(handle, "mousedown", onMouseDown);
-                self.fire("change");
-            }
-            
-            L.DomEvent.addListener(self.map, "mousemove", onMouseMove);
-            L.DomEvent.addListener(self.map, "mouseup", onMouseUp);
-        }
-        L.DomEvent.addListener(handle, "mousedown", onMouseDown);
     },
     
     _onMapResize: function() {
@@ -143,7 +149,7 @@ L.AreaSelect = L.Class.extend({
     
     _render: function() {
         var size = this.map.getSize();
-        var handleOffset = Math.round(this._nwHandle.offsetWidth/2);
+        // var handleOffset = Math.round(this._nwHandle.offsetWidth/2);
         
         var topBottomHeight = Math.round((size.y-this._height)/2);
         var leftRightWidth = Math.round((size.x-this._width)/2);
@@ -157,28 +163,59 @@ L.AreaSelect = L.Class.extend({
             element.style.right = dimension.right + "px";
         }
         
-        setDimensions(this._topShade, {width:size.x, height:topBottomHeight, top:0, left:0});
-        setDimensions(this._bottomShade, {width:size.x, height:topBottomHeight, bottom:0, left:0});
-        setDimensions(this._leftShade, {
-            width: leftRightWidth, 
-            height: size.y-(topBottomHeight*2), 
-            top: topBottomHeight, 
-            left: 0
-        });
-        setDimensions(this._rightShade, {
-            width: leftRightWidth, 
-            height: size.y-(topBottomHeight*2), 
-            top: topBottomHeight, 
-            right: 0
-        });
         
-        setDimensions(this._nwHandle, {left:leftRightWidth-handleOffset, top:topBottomHeight-7});
-        setDimensions(this._neHandle, {right:leftRightWidth-handleOffset, top:topBottomHeight-7});
-        setDimensions(this._swHandle, {left:leftRightWidth-handleOffset, bottom:topBottomHeight-7});
-        setDimensions(this._seHandle, {right:leftRightWidth-handleOffset, bottom:topBottomHeight-7});
+    setDimensions(this._topShade, { width: size.x, height: topBottomHeight, top: 0, left: 0 })
+    setDimensions(this._bottomShade, { width: size.x, height: topBottomHeight, bottom: 0, left: 0 })
+    setDimensions(this._leftShade, {
+      width: leftRightWidth,
+      height: size.y - topBottomHeight * 2,
+      top: topBottomHeight,
+      left: 0,
+    })
+    setDimensions(this._rightShade, {
+      width: leftRightWidth,
+      height: size.y - topBottomHeight * 2,
+      top: topBottomHeight,
+      right: 0,
+    })
+    setDimensions(this._topBorder, {
+      width: size.x - leftRightWidth * 2,
+      top: topBottomHeight,
+      height: 14,
+      left: leftRightWidth,
+    })
+    setDimensions(this._bottomBorder, {
+      width: size.x - leftRightWidth * 2,
+      top: size.y - topBottomHeight - 14,
+      height: 14,
+      left: leftRightWidth,
+    })
+    setDimensions(this._leftBorder, {
+      width: 14,
+      height: size.y - topBottomHeight * 2 - 28,
+      top: topBottomHeight + 14,
+      left: leftRightWidth,
+    })
+    setDimensions(this._rightBorder, {
+      width: 14,
+      height: size.y - topBottomHeight * 2 - 28,
+      top: topBottomHeight + 14,
+      left: size.x - leftRightWidth - 14,
+    })
+    setDimensions(this._nwHandle, { left: leftRightWidth - 14, top: topBottomHeight - 14 })
+    setDimensions(this._neHandle, { right: leftRightWidth - 14, top: topBottomHeight - 14 })
+    setDimensions(this._swHandle, { left: leftRightWidth - 14, bottom: topBottomHeight - 14 })
+    setDimensions(this._seHandle, { right: leftRightWidth - 14, bottom: topBottomHeight - 14 })
+
+    setDimensions(this._mmText, { left: leftRightWidth - 24, top: topBottomHeight - 20 })
+    setDimensions(this._mmHeightText, { left: leftRightWidth - 25, bottom: topBottomHeight - 22 })
+    setDimensions(this._mmWidthText, { right: leftRightWidth - 28, top: topBottomHeight - 20 })
+        
+        // setDimensions(this._nwHandle, {left:leftRightWidth-handleOffset, top:topBottomHeight-7});
     }
 });
 
 L.areaSelect = function(options) {
     return new L.AreaSelect(options);
 }
+export default L.areaSelect
